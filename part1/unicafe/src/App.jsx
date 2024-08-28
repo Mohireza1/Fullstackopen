c59bbb426c92a06998fb01dbe78d4b5f8705110c
Mohireza <mohireza50@gmail.com>
2024-08-28 19:23:44 +0330

    Exercises 1.8 - 1.11 completed

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -3,53 +3,63 @@ import { useState } from "react";
 // Simple button component
 const Button = ({ text, onClick }) => <button onClick={onClick}>{text}</button>;
 
-// Statistics displat
-const Vote = ({ feedback, option }) => (
-  <p>
-    {feedback[option][0]}: {feedback[option][1]}
-  </p>
+// Statistic line
+const StatisticLine = ({ name, value }) => (
+  <tr>
+    <th style={{ textAlign: "left" }}>{name}:</th>
+    <td>{value}</td>
+  </tr>
 );
+
+// Statistics display
+const Statistics = ({ feedback }) => {
+  const voteNums = [feedback.good, feedback.neutral, feedback.bad];
+  const totalVotes = voteNums.reduce((curr, vote) => curr + vote, 0);
+  const score = ((voteNums[0] - voteNums[2]) / totalVotes).toFixed(2);
+  const posPercent = ((voteNums[0] / totalVotes) * 100).toFixed(2);
+
+  if (!totalVotes) return <p>Not enough data</p>;
+  else
+    return (
+      <table>
+        <tbody>
+          <StatisticLine name={"Good"} value={voteNums[0]} />
+          <StatisticLine name={"Neutral"} value={voteNums[1]} />
+          <StatisticLine name={"Bad"} value={voteNums[2]} />
+          <StatisticLine name={"Total"} value={totalVotes} />
+          <StatisticLine name={"Score"} value={score} />
+          <StatisticLine name={"Positive Percentage"} value={posPercent} />
+        </tbody>
+      </table>
+    );
+};
 
 // A feedback app which lets you give feedback based on the three options "Good," "Neutral," and "Bad."
 // The data gets reset when the session is terminated.
 const App = () => {
   // The state. The feedback options can be changed if needed.
   const [feedback, setFeedback] = useState({
-    good: ["Delicious", 0],
-    neutral: ["Meh", 0],
-    bad: ["Awful", 0],
+    good: 0,
+    neutral: 0,
+    bad: 0,
   });
-
-  const voteNums = [feedback.good[1], feedback.neutral[1], feedback.bad[1]];
-  const totalVotes = voteNums.reduce((curr, vote) => curr + vote, 0);
-  const score = (voteNums[0] - voteNums[2]) / totalVotes;
-  const posPercent = (voteNums[0] / totalVotes) * 100;
 
   // Accepts a vote and increases it.
   const voteIncease = (state) => {
     setFeedback({
       ...feedback,
-      [state]: [feedback[state][0], feedback[state][1] + 1],
+      [state]: feedback[state] + 1,
     });
   };
 
   return (
-    <div>
+    <>
       <h1>Food Voting System</h1>
-      <Button text={feedback.good[0]} onClick={() => voteIncease("good")} />
-      <Button
-        text={feedback.neutral[0]}
-        onClick={() => voteIncease("neutral")}
-      />
-      <Button text={feedback.bad[0]} onClick={() => voteIncease("bad")} />
-      <h2>Statistics:</h2>
-      <Vote feedback={feedback} option={"good"} />
-      <Vote feedback={feedback} option={"neutral"} />
-      <Vote feedback={feedback} option={"bad"} />
-      <p>Total: {totalVotes}</p>
-      <p>Score: {isNaN(score) ? 0 : score.toFixed(2)}</p>
-      <p>Positive Percent: {isNaN(posPercent) ? 0 : posPercent.toFixed(2)}%</p>
-    </div>
+      <Button text={"Good"} onClick={() => voteIncease("good")} />
+      <Button text={"Neutral"} onClick={() => voteIncease("neutral")} />
+      <Button text={"Bad"} onClick={() => voteIncease("bad")} />
+      <Statistics feedback={feedback} />
+    </>
   );
 };
 
