65e514f08f576af9cdc737470879d6b827351197
Mohireza <mohireza50@gmail.com>
2024-09-05 22:26:10 +0330

    2.9 Complete

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,42 @@
 import { useState } from "react";
 
-const Entry = ({ person }) => <p>{person.name}</p>;
+const Entry = ({ person }) => (
+  <p>
+    {person.name}: {person.num}
+  </p>
+);
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas", key: 1 }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", num: "040-123456", key: 1 },
+    { name: "Ada Lovelace", num: "39-44-5323523", key: 2 },
+    { name: "Dan Abramov", num: "12-43-234345", key: 3 },
+    { name: "Mary Poppendieck", num: "39-23-6423122", key: 4 },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNum, setNewNum] = useState("");
+
+  const [searchPhrase, setSearchPhrase] = useState("");
+
+  const results =
+    searchPhrase.length > 0
+      ? persons.filter((per) => per.name.toLowerCase().includes(searchPhrase))
+      : persons;
 
   const handleName = (e) => {
     setNewName(e.target.value);
   };
+  const handleNum = (e) => {
+    setNewNum(e.target.value);
+  };
 
   const addPerson = (e) => {
     e.preventDefault();
-    const nameObj = { name: newName, key: persons.length + 1 };
+    const nameObj = {
+      name: newName,
+      num: String(newNum),
+      key: persons.length + 1,
+    };
 
     if (persons.filter((person) => person.name === nameObj.name).length > 0) {
       alert(`${nameObj.name} already exists!`);
@@ -21,12 +45,20 @@ const App = () => {
     }
   };
 
+  const search = (e) => {
+    setSearchPhrase(e.target.value.toLowerCase());
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      search: <input onChange={search}></input>
+      <h2>Add New</h2>
       <form>
         <div>
           name: <input value={newName} onChange={handleName} />
+          <br />
+          number: <input value={newNum} onChange={handleNum} />
         </div>
         <div>
           <button type="submit" onClick={addPerson}>
@@ -35,7 +67,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((per) => (
+      {results.map((per) => (
         <Entry key={per.key} person={per} />
       ))}
     </div>
